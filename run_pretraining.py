074ea0d46f09a86778eecf1108b18d1a7194588e
Ubuntu <ubuntu@ip-172-31-26-184.us-east-2.compute.internal>
2020-01-07 18:57:45 +0000

    Fixed some imports

--- run_pretraining.py
+++ run_pretraining.py
@@ -23,87 +23,88 @@ from . import modeling  # Ed: made it relative import
 from . import optimization
 import tensorflow as tf
 
-flags = tf.flags
+if False:
+    flags = tf.flags
 
-FLAGS = flags.FLAGS
+    FLAGS = flags.FLAGS
 
-## Required parameters
-flags.DEFINE_string(
-    "bert_config_file", None,
-    "The config json file corresponding to the pre-trained BERT model. "
-    "This specifies the model architecture.")
+    ## Required parameters
+    flags.DEFINE_string(
+        "bert_config_file", None,
+        "The config json file corresponding to the pre-trained BERT model. "
+        "This specifies the model architecture.")
 
-flags.DEFINE_string(
-    "input_file", None,
-    "Input TF example files (can be a glob or comma separated).")
+    flags.DEFINE_string(
+        "input_file", None,
+        "Input TF example files (can be a glob or comma separated).")
 
-flags.DEFINE_string(
-    "output_dir", None,
-    "The output directory where the model checkpoints will be written.")
+    flags.DEFINE_string(
+        "output_dir", None,
+        "The output directory where the model checkpoints will be written.")
 
-## Other parameters
-flags.DEFINE_string(
-    "init_checkpoint", None,
-    "Initial checkpoint (usually from a pre-trained BERT model).")
+    ## Other parameters
+    flags.DEFINE_string(
+        "init_checkpoint", None,
+        "Initial checkpoint (usually from a pre-trained BERT model).")
 
-flags.DEFINE_integer(
-    "max_seq_length", 128,
-    "The maximum total input sequence length after WordPiece tokenization. "
-    "Sequences longer than this will be truncated, and sequences shorter "
-    "than this will be padded. Must match data generation.")
+    flags.DEFINE_integer(
+        "max_seq_length", 128,
+        "The maximum total input sequence length after WordPiece tokenization. "
+        "Sequences longer than this will be truncated, and sequences shorter "
+        "than this will be padded. Must match data generation.")
 
-flags.DEFINE_integer(
-    "max_predictions_per_seq", 20,
-    "Maximum number of masked LM predictions per sequence. "
-    "Must match data generation.")
+    flags.DEFINE_integer(
+        "max_predictions_per_seq", 20,
+        "Maximum number of masked LM predictions per sequence. "
+        "Must match data generation.")
 
-flags.DEFINE_bool("do_train", False, "Whether to run training.")
+    flags.DEFINE_bool("do_train", False, "Whether to run training.")
 
-flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
+    flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
 
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+    flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
 
-flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
+    flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
 
-flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
+    flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
 
-flags.DEFINE_integer("num_train_steps", 100000, "Number of training steps.")
+    flags.DEFINE_integer("num_train_steps", 100000, "Number of training steps.")
 
-flags.DEFINE_integer("num_warmup_steps", 10000, "Number of warmup steps.")
+    flags.DEFINE_integer("num_warmup_steps", 10000, "Number of warmup steps.")
 
-flags.DEFINE_integer("save_checkpoints_steps", 1000,
-                     "How often to save the model checkpoint.")
+    flags.DEFINE_integer("save_checkpoints_steps", 1000,
+                         "How often to save the model checkpoint.")
 
-flags.DEFINE_integer("iterations_per_loop", 1000,
-                     "How many steps to make in each estimator call.")
+    flags.DEFINE_integer("iterations_per_loop", 1000,
+                         "How many steps to make in each estimator call.")
 
-flags.DEFINE_integer("max_eval_steps", 100, "Maximum number of eval steps.")
+    flags.DEFINE_integer("max_eval_steps", 100, "Maximum number of eval steps.")
 
-flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
+    flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
 
-tf.flags.DEFINE_string(
-    "tpu_name", None,
-    "The Cloud TPU to use for training. This should be either the name "
-    "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
-    "url.")
+    tf.flags.DEFINE_string(
+        "tpu_name", None,
+        "The Cloud TPU to use for training. This should be either the name "
+        "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
+        "url.")
 
-tf.flags.DEFINE_string(
-    "tpu_zone", None,
-    "[Optional] GCE zone where the Cloud TPU is located in. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+    tf.flags.DEFINE_string(
+        "tpu_zone", None,
+        "[Optional] GCE zone where the Cloud TPU is located in. If not "
+        "specified, we will attempt to automatically detect the GCE project from "
+        "metadata.")
 
-tf.flags.DEFINE_string(
-    "gcp_project", None,
-    "[Optional] Project name for the Cloud TPU-enabled project. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+    tf.flags.DEFINE_string(
+        "gcp_project", None,
+        "[Optional] Project name for the Cloud TPU-enabled project. If not "
+        "specified, we will attempt to automatically detect the GCE project from "
+        "metadata.")
 
-tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+    tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
 
-flags.DEFINE_integer(
-    "num_tpu_cores", 8,
-    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
+    flags.DEFINE_integer(
+        "num_tpu_cores", 8,
+        "Only used if `use_tpu` is True. Total number of TPU cores to use.")
 
 
 def model_fn_builder(bert_config, init_checkpoint, learning_rate,
@@ -121,12 +122,12 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
             tf.logging.info("  name = %s, shape = %s" % (name, features[name].shape))
 
         input_ids = features["input_ids"]
-        input_mask = features["input_mask"]
         segment_ids = features["segment_ids"]
-        masked_lm_positions = features["masked_lm_positions"]
-        masked_lm_ids = features["masked_lm_ids"]
-        masked_lm_weights = features["masked_lm_weights"]
-        next_sentence_labels = features["next_sentence_labels"]
+        input_mask = features.get("input_mask", None)
+        masked_lm_positions = features.get("masked_lm_positions", None)
+        masked_lm_ids = features.get("masked_lm_ids", None)
+        masked_lm_weights = features.get("masked_lm_weights", None)
+        next_sentence_labels = features.get("next_sentence_labels", None)
 
         is_training = (mode == tf.estimator.ModeKeys.TRAIN)
 
