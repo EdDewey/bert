ed72a611ea316609e95e6773b70aa2772b8eedf5
EdDewey <ed.dewey@gmail.com>
2020-01-07 14:41:17 -0500

    un-un-fixed flags

--- create_pretraining_data.py
+++ create_pretraining_data.py
@@ -26,49 +26,49 @@ import attrdict
 from . import tokenization
 import tensorflow as tf
 
-flags = tf.flags
+if False:
+    flags = tf.flags
 
-FLAGS = flags.FLAGS
+    FLAGS = flags.FLAGS
 
-flags.DEFINE_string("input_file", None,
-                    "Input raw text file (or comma-separated list of files).")
+    flags.DEFINE_string("input_file", None,
+                        "Input raw text file (or comma-separated list of files).")
 
-flags.DEFINE_string(
-    "output_file", None,
-    "Output TF example file (or comma-separated list of files).")
+    flags.DEFINE_string(
+        "output_file", None,
+        "Output TF example file (or comma-separated list of files).")
 
-flags.DEFINE_string("vocab_file", None,
-                    "The vocabulary file that the BERT model was trained on.")
+    flags.DEFINE_string("vocab_file", None,
+                        "The vocabulary file that the BERT model was trained on.")
 
-flags.DEFINE_bool(
-    "do_lower_case", True,
-    "Whether to lower case the input text. Should be True for uncased "
-    "models and False for cased models.")
+    flags.DEFINE_bool(
+        "do_lower_case", True,
+        "Whether to lower case the input text. Should be True for uncased "
+        "models and False for cased models.")
 
-flags.DEFINE_bool(
-    "do_whole_word_mask", False,
-    "Whether to use whole word masking rather than per-WordPiece masking.")
+    flags.DEFINE_bool(
+        "do_whole_word_mask", False,
+        "Whether to use whole word masking rather than per-WordPiece masking.")
 
-flags.DEFINE_integer("max_seq_length", 128, "Maximum sequence length.")
+    flags.DEFINE_integer("max_seq_length", 128, "Maximum sequence length.")
 
-flags.DEFINE_integer("max_predictions_per_seq", 20,
-                     "Maximum number of masked LM predictions per sequence.")
+    flags.DEFINE_integer("max_predictions_per_seq", 20,
+                         "Maximum number of masked LM predictions per sequence.")
 
-flags.DEFINE_integer("random_seed", 12345, "Random seed for data generation.")
+    flags.DEFINE_integer("random_seed", 12345, "Random seed for data generation.")
 
-flags.DEFINE_integer(
-    "dupe_factor", 10,
-    "Number of times to duplicate the input data (with different masks).")
+    flags.DEFINE_integer(
+        "dupe_factor", 10,
+        "Number of times to duplicate the input data (with different masks).")
 
-flags.DEFINE_float("masked_lm_prob", 0.15, "Masked LM probability.")
+    flags.DEFINE_float("masked_lm_prob", 0.15, "Masked LM probability.")
 
-flags.DEFINE_float(
-    "short_seq_prob", 0.1,
-    "Probability of creating sequences which are shorter than the "
-    "maximum length.")
-
-if True:
-    flags = attrdict.Attrdict(
+    flags.DEFINE_float(
+        "short_seq_prob", 0.1,
+        "Probability of creating sequences which are shorter than the "
+        "maximum length.")
+else:
+    FLAGS = attrdict.Attrdict(
         {
             'input_file': 'bert/bert_training_example.txt',  # Careful about relative paths, this is probably going
             # to fail
